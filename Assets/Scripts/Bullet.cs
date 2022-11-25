568fb1a7f25197cbc15b2902af0123e2a3f4fbbf
burakekinci <burakekinci76@yandex.com>
2022-11-26 02:18:38 +0300

    Atış Mekaniği eklendi

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float damagePower = 10f;
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+        StartCoroutine(SelfDestruct());
+    }
+
+    void Update(){
+        float angle = Mathf.Atan2(rb.velocity.y, rb.velocity.z) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.right);
+    }
+
+    IEnumerator SelfDestruct(){
+        yield return new WaitForSeconds(10f);
+        Destroy(gameObject);
+    }
+
+
+    void OnTriggerEnter(Collider other) {
+        if(other.CompareTag("Enemy")){
+            //TODO:damagePower miktarında hasar ver
+            Debug.Log("temas");
+            Destroy(other.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/ShootingController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingController : MonoBehaviour
+{
+    public GameObject bulletProjectile;
+    public Transform firePointTransform;
+
+    public float fireCooldownInSeconds = 1f;
+    public float launchVelocity = 750f;
+    private bool allowFire = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1") && allowFire){
+            StartCoroutine(Fire());
+        }   
+    }
+
+    IEnumerator Fire(){
+        allowFire=false;
+        GameObject launchedBullet =  Instantiate(bulletProjectile,firePointTransform.transform.position,firePointTransform.transform.rotation);
+        launchedBullet.GetComponent<Rigidbody>().AddRelativeForce(new Vector3(0,launchVelocity,0),ForceMode.Impulse);
+        yield return new WaitForSeconds(fireCooldownInSeconds);
+        allowFire=true;
+    }
+}
