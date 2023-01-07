05860623a1a565caa020c965513c47c54a9149cf
burakekinci <burakekinci76@yandex.com>
2023-01-07 17:07:40 +0300

    Update ShipItemController.cs

--- Assets/Scripts/ShipItemController.cs
+++ Assets/Scripts/ShipItemController.cs
@@ -40,7 +40,6 @@ public class ShipItemController : MonoBehaviour
             GameObject.Find("InventoryMenu").GetComponent<InventoryMenu>();
         shipPrice = GameStats.Instance.ShipPrices[itemId];
         upgradeButton = transform.Find("ShipItemUpgrade").gameObject;
-        UpdateShipProperties();
     }
 
     private void OnEnable()
